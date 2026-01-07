a69f00b51dda2b8f6a4ac5d933ea6e9aad41ec06
geng-yu <ss8611ss@hotmail.com>
2026-01-07 16:50:17 +0800

    Update day1.py

--- day1.py
+++ day1.py
@@ -74,9 +74,9 @@ def show():
     
     # [修正] 更新飯店名稱與地址
     with st.container(border=True):
-        st.text(" **Daiwa Roynet Hotel KANAZAWAEKI-NISHIGUCHI** ")
+        st.text("Daiwa Roynet Hotel KANAZAWAEKI-NISHIGUCHI")
         st.text("ダイワロイネットホテル 金沢駅西口")
-        st.markdown("---")
+        #st.markdown("---")
         st.text("📍 日：〒920-0031 石川県金沢市広岡1-12-17")
         st.text("📍 英：1-12-17, Hirooka, Kanazawa-shi, Ishikawa 920-0031")
         
